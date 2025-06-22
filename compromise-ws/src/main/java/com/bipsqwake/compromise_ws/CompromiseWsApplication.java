6a66fe1789bced084dcd963f8f1c43e9ae68eaef
bipsqwake <bipsqwake42@gmail.com>
2025-06-22 22:53:09 +0300

    Network

--- compromise-ws/src/main/java/com/bipsqwake/compromise_ws/CompromiseWsApplication.java
+++ compromise-ws/src/main/java/com/bipsqwake/compromise_ws/CompromiseWsApplication.java
@@ -2,10 +2,12 @@ package com.bipsqwake.compromise_ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableDiscoveryClient
 @EnableScheduling
 @EnableRetry
 @EnableAspectJAutoProxy 
